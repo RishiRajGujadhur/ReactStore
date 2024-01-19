e3b748a1a0dcd0869ecd7d66182198e6fec0d3ca
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-01-19 11:20:12 +0400

    Updated COR endpoint

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(opt => {
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000/");
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
 });
 
 app.UseAuthorization();
