aa7b99486abed4dae706a84c983e0990b10a22bd
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-02-23 17:56:35 +0400

    Add user authentication param to LikeBL.cs

--- API/BL/LikeBL.cs
+++ API/BL/LikeBL.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Identity;
@@ -9,10 +10,10 @@ namespace API.BL
     {
         Task<IEnumerable<Like>> GetLikes();
         Task<Like> GetLike(int id);
-        Task<bool> UserLikedProduct(int productId);
-        Task UnlikeProduct(int productId);
-        Task CreateLike(int productId);
-        Task<IEnumerable<Product>> GetLikedProducts();
+        Task<bool> UserLikedProduct(int productId, ClaimsPrincipal User);
+        Task UnlikeProduct(int productId, ClaimsPrincipal User);
+        Task CreateLike(int productId, ClaimsPrincipal User);
+        Task<IEnumerable<Product>> GetLikedProducts(ClaimsPrincipal User);
     }
 
     public class LikeBL : ILikeBL
@@ -26,20 +27,51 @@ namespace API.BL
             _userManager = userManager;
         }
 
-        public Task CreateLike(int productId)
+        public async Task CreateLike(int productId, ClaimsPrincipal User)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            // Check if the user has already liked the product
+            var existingLike = await _context.Likes
+                .FirstOrDefaultAsync(l => l.UserId == user.Id && l.ProductId == productId);
+
+            if (existingLike != null)
+            {
+                //return BadRequest("User has already liked this product.");
+            }
+
+            Like like = new()
+            {
+                UserId = user.Id,
+                ProductId = productId
+            };
+
+            _context.Likes.Add(like);
+            await _context.SaveChangesAsync();
+
+            //return NoContent();
         }
 
         public async Task<Like> GetLike(int id)
         {
-           var like = await _context.Likes.FindAsync(id) ?? throw new KeyNotFoundException();
-           return like;
+            var like = await _context.Likes.FindAsync(id) ?? throw new KeyNotFoundException();
+            return like;
         }
 
-        public async Task<IEnumerable<Product>> GetLikedProducts()
+        public async Task<IEnumerable<Product>> GetLikedProducts(ClaimsPrincipal User)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var likedProductIds = await _context.Likes
+                .Where(l => l.UserId == user.Id)
+                .Select(l => l.ProductId)
+                .ToListAsync();
+
+            var likedProducts = await _context.Products
+                .Where(p => likedProductIds.Contains(p.Id))
+                .ToListAsync();
+
+            return likedProducts;
         }
 
         public async Task<IEnumerable<Like>> GetLikes()
@@ -47,14 +79,33 @@ namespace API.BL
             return await _context.Likes.ToListAsync();
         }
 
-        public Task UnlikeProduct(int productId)
+        public async Task UnlikeProduct(int productId, ClaimsPrincipal User)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            // Check if the user has liked the specified product
+            var like = await _context.Likes
+                .FirstOrDefaultAsync(l => l.ProductId == productId && l.UserId == user.Id);
+
+            if (like == null)
+            {
+                // If the like entry doesn't exist, return NotFound or BadRequest, depending on your preference.
+                //return Throw($"User {user.Id} never liked product {productId}");
+            }
+
+            // Remove the like entry from the database
+            _context.Likes.Remove(like);
+            await _context.SaveChangesAsync();
+
         }
 
-        public Task<bool> UserLikedProduct(int productId)
+        public async Task<bool> UserLikedProduct(int productId, ClaimsPrincipal User)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            // Check if the user has liked the specified product
+            var like = await _context.Likes
+                .FirstOrDefaultAsync(l => l.ProductId == productId && l.UserId == user.Id);
+            return like != null;
         }
     }
 }
