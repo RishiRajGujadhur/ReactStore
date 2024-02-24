289f63adfa8e35f814ee8fa50b0051e4e55fbbf5
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-02-24 12:03:22 +0400

    Add login, registration, and user retrieval functionality

--- API/BL/AccountBL.cs
+++ API/BL/AccountBL.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +9,11 @@ namespace API.BL
 {
     public interface IAccountBL
     {
-      
+        Task<UserDto> Login(LoginDto loginDto);
+        Task<UserAddress> GetSavedAddress();
+        Task<List<User>> GetAllUsers();
+        Task RegisterUser(RegisterDto registerDto);
+        Task<UserDto> GetCurrentUser();
     }
 
     public class AccountBL : IAccountBL
@@ -20,6 +25,31 @@ namespace API.BL
         {
             _context = context;
             _userManager = userManager;
-        } 
+        }
+
+        public Task<UserDto> Login(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserAddress> GetSavedAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterUser(RegisterDto registerDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
