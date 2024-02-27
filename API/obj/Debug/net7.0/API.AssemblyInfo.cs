93e09d83c1351e4773c7bcd2a72504a1216d95a1
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-02-27 16:56:11 +0400

    removed unused

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f252a388a0b4e29ccda27dac97fb172771a1f9a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ac64c021a43a40139af532981de8024b2fcee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
