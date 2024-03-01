a5f68110845b6ec28ea4f586f161c6a77d66cfe5
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-03-01 19:12:02 +0400

    Logger Update

--- API/Extentions/LoggerExtention.cs
+++ API/Extentions/LoggerExtention.cs
@@ -2,9 +2,9 @@ namespace API.Extentions
 {
     public static class LoggerExtention
     {
-        public static string AddErrorDetails(Exception ex, string username, string message = "")
+        public static string AddErrorDetails(Exception ex, string username, string message = "",  string methodName = "")
         {
-            return $"{message} - {username} : {DateTime.UtcNow} - {ex.InnerException.Message}, {ex.InnerException.InnerException.Message}, {ex.Message}";
+            return $"{message} - {username} : {DateTime.UtcNow} - {ex.InnerException.Message}, {ex.InnerException.InnerException.Message}, {ex.Message}, method: {methodName}";
         }
     }
 }
